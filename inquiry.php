b411737cc22e71528a70acaff118305940d4bd18
edetchi <contacts@edetchi.com>
2017-09-26 12:00:48 +0900

    inquiry.phpにformを追加
    入力エリアをフォーカスしやすいようにlabel要素にfor属性を追加

--- inquiry.php
+++ inquiry.php
@@ -1,6 +1,23 @@
 <?php $page_title = "お問い合わせ"; ?>
 <?php require "header.php"; ?>
     <p>
-      準備中
+      お問い合わせは以下よりお願いします
     </p>
+    <form action="inquiry.php" method="post">
+        <div>
+            <label for="namae">お名前: </label>
+            <input type="text" name="uname" id="namae" size="30">
+        </div>
+        <div>
+            <label for="meado">メールアドレス: </label>
+            <input type="email" name="email" id="meado" size="30">
+        </div>
+        <div>
+            <label for="toiawase">お問い合わせ内容: </label>
+            <textarea name="body" rows="5" id="toiawase" cols="20"></textarea>
+        </div>
+        <div>
+            <input type="submit" name="send" value="送信する">
+        </div>
+    </form>
 <?php require "footer.php"; ?>
--- memo.md
+++ memo.md
@@ -1,9 +1,19 @@
+# HTML
+ - form
+  - inputの識別子（id）をlabelのforで指定すると、label要素をクリックした時に、inputがフォーカスされるので指定しておくと便利かも
+   ```html
+   <label for="lastname">名前: </label>
+   <input type="text" name="mei" id="lastname">
+   ```
+   - textareaの属性: rows="縦文字数（行数）" cols="横文字数"
 # CSS
  - reset.cssは、自分の適用させたいスタイルより前に読み込むこと
  - CSSプロパティの並び順は、**視覚整形→ボックス→背景→フォント→コンテンツ**がベター
 
 # PHP
  - include/require構文: require構文はファイルが存在しなかったらエラー出力で停止、include構文はそのまま続行
+ - include/require構文を呼び出すファイルで、変数を、呼び出すファイルに反映させたいときは、呼び出す前に変数の定義をしておく
  - <?= 出力したい文字列 ?>: これはどのphpバージョンでもサポートしている（<?php echo 文字列; ?>と同義）
+
 # Linux
  - 改行: LinuxではLF（Line Feed）、MacではCR（Carriage Return）
