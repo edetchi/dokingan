eb866273acfe9bfe8b503f29f9dae415a7d862ac
edetchi <contacts@edetchi.com>
2017-09-26 14:28:52 +0900

    フォーム内容を管理者にメールで送信
    [send]がある時に、mb_send_mail()関数を実行

--- inquiry.php
+++ inquiry.php
@@ -1,10 +1,41 @@
 <?php
+/*=========================================
+    送信モードの判別開始
+=========================================*/
 if(isset($_REQUEST["send"])){
     echo "送信モード<br>";
     var_dump($_REQUEST);
+/*----------------------------------------------------------------------------------
+    管理者向けメールの送信、mb_send_mail(送信先, 件名, 本文, ヘッダ);
+----------------------------------------------------------------------------------*/
+    //メール送信用に、言語と文字コードの指定
+    mb_language("Japanese"); 
+    mb_internal_encoding("UTF-8");
+    //本文用の変数を初期化して、条件に合致するたびに本文を追加していく
+    $mail_body = "";
+    if(isset($_REQUEST["uname"])){
+        $mail_body .= "名前: ";
+        $mail_body .= $_REQUEST['uname'] . "\n";
+    }
+    if(isset($_REQUEST["email"])){
+        $mail_body .= "メアド: ";
+        $mail_body .= $_REQUEST['email'] . "\n";
+    }
+    if(isset($_REQUEST["body"])){
+        $mail_body .= "本文: ";
+        $mail_body .= $_REQUEST['body'] . "\n";
+    }
+    //送信実行
+    $subject = "新規問い合わせ";
+    $admin_email = "suteado@edetchi.com";
+    $add_header = "From:" . $admin_email;
+    $result = mb_send_mail($admin_email, $subject, $mail_body, $add_header);
 } else {
     echo "直見";
 }
+/*=========================================
+    送信モードの判別終了
+=========================================*/
 ?>
 <?php $page_title = "お問い合わせ"; ?>
 <?php require "header.php"; ?>
