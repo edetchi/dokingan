7a70c6afa4ae4952c0762dc6148cb7cfeee39582
edetchi <contacts@edetchi.com>
2017-09-28 15:45:18 +0900

    記事投稿の管理画面をadminディレクトリ以下に作成開始
    共通部分header.php, footer.phpを作成

---
+++ admin/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title><?= $page_title; ?> | マイアカウント</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <h1><?= "$page_title"; ?></h1>
