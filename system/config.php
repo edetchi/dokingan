ab0c23265017bfda807b49fedb4ef563910f1d36
edetchi <contacts@edetchi.com>
2017-09-28 14:08:12 +0900

    共通処理をライブラリにまとめる
    systemディレクトリ以下にライブラリを格納し、.htaccessでアクセスを制限する
    
    config.phpにはシステムの環境設定をまとめて記載しておく

---
+++ system/config.php
@@ -0,0 +1,11 @@
+<?php
+/*----------------------------------------------------------------------------
+    データベースの環境設定
+----------------------------------------------------------------------------*/
+$db_type = "mysql";
+$db_host = "localhost";
+$db_name = "framerefugee";
+$db_user = "root";
+$db_pass = "root";
+$dsn = "{$db_type}:host={$db_host};dbname={$db_name};charset=utf8";
+?>
