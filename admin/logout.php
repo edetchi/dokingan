3490ab9163c464e1b0f7c1ec32ec4c7b1632fd35
edetchi <contacts@edetchi.com>
2017-09-30 20:58:28 +0900

    ログアウト機能作成

---
+++ admin/logout.php
@@ -0,0 +1,15 @@
+<?php require_once("../system/admin_common.php"); ?>
+<?php
+$_SESSION["user_id"] = array();
+if (isset($_COOKIE["PHPSESSID"])) {
+  setcookie("PHPSESSID", "", time()-3600, "/");
+}
+session_destroy();
+$page_message = "ログアウトしました"
+?>
+<?php $page_title = "ログアウト";?>
+<?php require("header.php"); ?>
+    <p>
+      <?= he($page_message) ?>
+    </p>
+<?php require("footer.php"); ?>
