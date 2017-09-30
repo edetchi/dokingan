4690ca689f2dfe82d376a06eb00448115c1123da
edetchi <contacts@edetchi.com>
2017-09-30 17:08:51 +0900

    修正モード時にデータベースから修正元のデータをinput欄に挿入

--- admin/item_edit.php
+++ admin/item_edit.php
@@ -4,13 +4,44 @@
     変数をホワイトリスト化
 -----------------------------------------------------------------------------*/
 //$_REQUEST[]の取りうるキーを限定する
-$whitelists = array("mode", "item_name", "item_comment", "send");
+$whitelists = array("mode", "post_id", "item_name", "item_comment", "send");
 $request = whitelist($whitelists);
 /*-----------------------------------------------------------------------------
     メッセージの初期化
 -----------------------------------------------------------------------------*/
 $page_message = "";
 $error_message = "";
+/*-----------------------------------------------------------------------------
+    動作モードを$modeに格納（空白→新規、chnage→修正）
+-----------------------------------------------------------------------------*/
+$mode = $request["mode"];
+/*-----------------------------------------------------------------------------
+    フォームの初期化
+-----------------------------------------------------------------------------*/
+$form = array();
+$form["post_id"] = $request["post_id"];
+$form["item_name"] = $request["item_name"];
+$form["item_comment"] = $request["item_comment"];
+/*-----------------------------------------------------------------------------
+    修正モード時の処理
+-----------------------------------------------------------------------------*/
+if (!isset($request["send"]) && $mode == "change") {
+	try {
+		$sql = "select * from posts where post_id = :post_id";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(":post_id", $request["post_id"], PDO::PARAM_INT);
+		$stmt->execute();
+		$row_post = $stmt->fetch(PDO::FETCH_ASSOC);
+			if ($row_post) {
+				$form["item_name"] = $row_post["post_title"];
+				$form["item_comment"] = $row_post["post_content"];
+			} else {
+				die("異常なアクセスですkk");
+			}
+	} catch (PDOException $e) {
+		die("エラー: " . $e->getMessage());
+	}
+}
 /*-----------------------------------------------------------------------------
     フォーム項目のエラーチェック
 -----------------------------------------------------------------------------*/
@@ -46,19 +77,27 @@ if (isset($request["send"]) && $error_message == "") {
 <?php $page_title = "フレーム編集";?>
 <?php require("header.php"); ?>
 	<p>
+		<a href="item_list.php">一覧へ戻る</a>
+	</p>
+	<p>
 		<?= he($page_message) ?>
 	</p>
 	<p class="attention">
 		<?= nl2br(he($error_message)) ?>
 	</p>
+	<?php if ($mode == "change"): ?>
+    <p>
+      記事ID[<?= he($form["post_id"]); ?>]を修正しています
+    </p>
+	<?php endif; ?>
 	<form action="item_edit.php" method="post">
 		<div>
 			<label for="hure-mumei">フレーム名<span class="attention">【必須】</span></label>
-			<input type="text" name="item_name" id="hure-mumei" size="30" value="<?= he($request["item_name"]); ?>">
+			<input type="text" name="item_name" id="hure-mumei" size="30" value="<?= he($form["item_name"]); ?>">
 		</div>
 		<div>
 			<label for="komento">コメント<span class="attention">【必須】</span></label>
-			<textarea name="item_comment" id="komento" rows="5" cols="20"><?= he($request["item_comment"]); ?></textarea>
+			<textarea name="item_comment" id="komento" rows="5" cols="20"><?= he($form["item_comment"]); ?></textarea>
 		</div>
 		<div>
 			<input type="submit" name="send" value="送信する">
