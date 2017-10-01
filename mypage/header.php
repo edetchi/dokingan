4443e40bb6a5271fdbd218a6601d2812f7e30d9c
edetchi <contacts@edetchi.com>
2017-10-01 12:22:36 +0900

    マイページのヘッダーに表示するリンクをURLに応じて変化させる
    URLに応じてマイページトップ画面へのリンク、ログアウトリンクを表示

--- mypage/header.php
+++ mypage/header.php
@@ -2,9 +2,19 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8" />
-    <title><?= $page_title; ?> | マイアカウント</title>
+    <title><?= $page_title; ?> | マイページ</title>
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
     <h1><?= "$page_title"; ?></h1>
+    <?php if (!$is_mypage_top && $login_flag): ?>
+        <div>
+          <a href=".">マイページ</a>
+        </div>
+      <?php endif; ?>
+    <?php if ($login_flag): ?>
+        <div>
+          <a href="logout.php">ログアウト</a>
+        </div>
+    <?php endif; ?>
--- mypage/index.php
+++ mypage/index.php
@@ -1,9 +1,12 @@
 <?php require_once("../system/common.php"); ?>
-<?php $page_title = "トップページ";?>
+<?php $page_title = "マイページ";?>
 <?php require("header.php"); ?>
 	<ul>
 		<li>
 			<a href="item_list.php">フレーム管理</a>
+		</li>
+		<li>
+			<a href="account.php">アカウント設定</a>
 		</li>
 	</ul>
 <?php require("footer.php"); ?>
