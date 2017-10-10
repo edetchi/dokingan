0b0e8f948672054c7cd1e00d4922a94eddd947b1
edetchi <contacts@edetchi.com>
2017-10-10 09:22:37 +0900

    fontawesomeインストール
    fontsディレクトリ以下のファイルはエラー表示がでるので念のため追加、なくても動くことは動く

--- mypage/header.php
+++ mypage/header.php
@@ -6,6 +6,7 @@
     <script src="../js/jquery-3.2.1.min.js"></script>
     <script src="../js/script.js"></script>
     <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
