4b6e4c5c9bcd55d52764f7f12eb660a806f26d6b
edetchi <contacts@edetchi.com>
2017-10-16 15:49:50 +0900

    mypage/footer.phpを上のfooter.phpと同じに

--- mypage/footer.php
+++ mypage/footer.php
@@ -1,22 +1,26 @@
 <footer>
+  <div class="footer-nav__layout">
   <ul class="footer-nav">
-    <li>
+    <li class="footer-nav__item">
       <i class="fa fa-chevron-right" aria-hidden="true"></i>
       <a href="#">このサイトの使い方</a>
     </li>
-    <li>
+    <li class="footer-nav__item">
       <i class="fa fa-chevron-right" aria-hidden="true"></i>
       <a href="#">レンズを薄く仕上げるには</a>
     </li>
-    <li>
+    <li class="footer-nav__item">
       <i class="fa fa-chevron-right" aria-hidden="true"></i>
       <a href="#"> 管理人プロフィール</a>
     </li>
-    <li>
+    <li class="footer-nav__item">
       <i class="fa fa-chevron-right" aria-hidden="true"></i>
       <a href="inquiry.php"> 新機能要望・トラブルシュート</a>
     </li>
   </ul><!--.footer-nav-->
-  <small class="copy-right">&copy; 2017 Dokingan.com.</small><!--.copy-righrt-->
+  </div>
+  <div class="copy-right">
+    <small class="copy-right__declare">&copy; 2017 Dokingan.com.</small><!--.copy-righrt-->
+  </div>
 </footer>
 </div><!--.wrapper-->
