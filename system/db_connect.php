dc671e55e92232c7f25a1c3ce3922b59160ae390
edetchi <contacts@edetchi.com>
2017-10-19 22:40:09 +0900

    sqlite関係のファイルを削除

--- system/db_connect.php
+++ system/db_connect.php
@@ -3,9 +3,10 @@
     データベースへの接続
 -----------------------------------------------------------------------------*/
 //MySQL
-
 try {
-	$pdo = new PDO($dsn, $db_user, $db_pass);
+  $pdo = new PDO($dsn, $db_user, $db_pass);
+  //SQLite
+  //$pdo = new PDO('sqlite:../sqlite3/dokingan.sqlite3');
 	//エラーモード設定
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	//プリペアドステートメント用意
@@ -16,17 +17,4 @@ try {
 	//エラー発生時処理停止してエラー表示
 	die("エラー: " . $e->getMessage());
 }
-
-//SQLite
-/*
-try {
-    $pdo = new SQLite3('..sqlite//dokingan.sqlite3');
-    if (!$pdo) {
-      die('接続失敗です。'.$sqliteerror);
-    }
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-    exit;
-}
-*/
 ?>
