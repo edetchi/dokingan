2fb864a1af47bbc8dd10c221587a4a79df7365f4
edetchi <contacts@edetchi.com>
2017-12-20 11:41:27 +0900

    favicon設定

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <link href="https://fonts.googleapis.com/earlyaccess/mplus1p.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 <body>
 <div class="wrapper">
