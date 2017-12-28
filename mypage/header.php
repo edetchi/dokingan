c9d3243fb258b97fd3689bf2d9af5e4fe19e7477
edetchi <contacts@edetchi.com>
2017-12-28 17:44:24 +0900

    jqueryをCDNに変更

--- mypage/header.php
+++ mypage/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= $page_title; ?> | <?= SITE_NAME ?></title>
-  <script src="../js/jquery-3.2.1.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="../js/script.js"></script>
   <link rel="stylesheet" href="../css/reset.css">
   <link rel="stylesheet" href="../css/font-awesome.min.css">
