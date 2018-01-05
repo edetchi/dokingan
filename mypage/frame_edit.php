48ef6c3bf352a0bdc45a8163c98eac70d95e8cf8
edetchi <contacts@edetchi.com>
2018-01-06 00:42:36 +0900

    画像複数アップロードの実装完了、データベース登録おk

--- mypage/frame_edit.php
+++ mypage/frame_edit.php
@@ -6,9 +6,9 @@ require_once("../system/common.php");
 //$_REQUEST[]の取りうるキーを限定する
 $whitelists = array("mode", "frame_id","frame_poster_id","frame_price","frame_image","frame_link","frame_lens_width","frame_lens_height","frame_bridge_width","frame_temple_length","frame_frame_width", "send");
 $request = whitelist($whitelists);
-/*-----------------------------------------------------------------------------
-    画像のアップロードがある時だけ変数に格納
------------------------------------------------------------------------------*/
+/*=============================================================================
+    <<画像のアップロードがある時に行う事前処理
+=============================================================================*/
 //エラー避け
 $_FILES["frame_image"] = (!empty($_FILES["frame_image"])) ? $_FILES["frame_image"] : "";
 //画像アップ時に変数に格納
@@ -23,6 +23,9 @@ if ($_FILES["frame_image"]) {
   print("<br>");
   var_export($image_tmp);
   print("<br><br>");
+/*-----------------------------------------------------------------------------
+    画像の名前用配列を用意
+-----------------------------------------------------------------------------*/
   $image_names = array();
   foreach ($image["name"] as $key => $value) {
     //画像の拡張子をファイル名から抽出
@@ -32,23 +35,23 @@ if ($_FILES["frame_image"]) {
     //拡張子をjpgにして時間をプリフィックスにして画像名を作成、ベースのファイル名がない時は空白
     $image_name = (!empty($image_basename)) ? date("YmdHis") . $image_basename . ".jpg" : "";
     //var_export($image_basename);
-    $image_names[] = $image_name;
+    //最後のinputが空白の場合、$image_namesに名前を追加しない
+    if (!empty($image_basename)) $image_names[] = $image_name;
   }
   print('$image_names');
   print("<br>");
   var_export($image_names);
   print("<br><br>");
-  /*
-  //画像の拡張子をファイル名から抽出
-  $image_str_extension = substr($image["name"], strrpos($image["name"], '.') + 1);
-  var_export($image_str_extension);
-  //画像から拡張子を除いたファイル名をゲット
-  $image_basename = str_replace(".{$image_str_extension}", "", $image["name"]);
-  //拡張子をjpgにして時間をプリフィックスにして画像名を作成
-  $image_name = date("YmdHis") . $image_basename . ".jpg";
-  //var_export($image_basename);
-  */
+  //データベース登録用に配列を文字列に変換し、変数に格納
+  print('$image_names_imploded');
+  print("<br>");
+  $image_names_imploded = toggleStrArray($image_names);
+  var_dump($image_names_imploded);
+  print("<br><br>");
 }
+/*=============================================================================
+    画像のアップロードがある時に行う事前処理>>
+=============================================================================*/
 /*-----------------------------------------------------------------------------
     メッセージの初期化
 -----------------------------------------------------------------------------*/
@@ -79,20 +82,20 @@ try {
   die("エラー: " . $e->getMessage());
 }
 /*-----------------------------------------------------------------------------
-    フォームの初期化
+    投稿者以外がフレームデータを編集、削除しようとすると転送、select後の最初の処理に実施
+-----------------------------------------------------------------------------*/
+if (!empty($mode) && $row_frame["frame_poster_id"] !== $_SESSION["user_id"]) {
+  header("Location: frame_list.php");
+  exit;
+}
+/*-----------------------------------------------------------------------------
+    frame_idで該当するデータがある時、画像とメッセージ系の初期処理
 -----------------------------------------------------------------------------*/
 if ($row_frame/* && $form["frame_poster_id"] === $_SESSION["user_id"]*/) {
   //古い画像削除用にファイル名をセッションに取得しておく
   $_SESSION["old_image"] = $row_frame["frame_image"];
   //フレーム編集の初見時のコメントセット
   if (empty($request["send"])) $page_msgs[] = "フレームID【{$request['frame_id']}】を修正しています";
-}
-/*-----------------------------------------------------------------------------
-    投稿者以外がフレームデータを編集、削除できないようにする
------------------------------------------------------------------------------*/
-if (!empty($mode) && $row_frame["frame_poster_id"] !== $_SESSION["user_id"]) {
-  header("Location: frame_list.php");
-  exit;
 }
 /*=============================================================================
     getでframe_idの値が与えられている時、各値を取得>>
@@ -120,8 +123,9 @@ if ($mode == "delete"/* && $form["frame_poster_id"] === $_SESSION["user_id"]*/) 
     die("エラー; " . $e->getMessage());
   }
   //古い画像削除
-  unlink("../images/frames/{$_SESSION["old_image"]}");
-  unlink("../images/frames/thumb_{$_SESSION["old_image"]}");
+  deleteImages($_SESSION["old_image"]);
+  //unlink("../images/frames/{$_SESSION["old_image"]}");
+  //unlink("../images/frames/thumb_{$_SESSION["old_image"]}");
   header("Location: frame_list.php");
   exit;
 }
@@ -136,11 +140,14 @@ if (isset($request["send"])) {
 /*-----------------------------------------------------------------------------
     画像のチェック
 -----------------------------------------------------------------------------*/
+  //画像のアップロードがある時の処理開始
+  //画像のアップロードがある時、$_FILES["name"]["error"]で得られる連想配列の値が0となる
   print('array_search(0, $image["error"])');
   print("<br>");
   var_export(array_search(0, $image["error"]));
   print("<br><br>");
   if (array_search(0, $image["error"]) !== false) {
+    //拡張子のバリデーションチェック
     foreach ($image_tmp as $key => $value) {
       if (!empty($value)) {
         //文字列から抽出した拡張子($image_str_extension)と$_FILE["type"]の拡張子は偽装できるので、getimagesizeで本当の拡張子ゲット
@@ -150,21 +157,23 @@ if (isset($request["send"])) {
         print("<br><br>");
         $image_extension = str_replace("image/", "", getimagesize($value)["mime"]);
         //var_dump($image_extension);
+        //許可された拡張子か関数でチェック
         if (imageExtensionFlag($image_extension) == 0) {
           $error_msgs[] = "【{$image['name'][$key]}】拡張子は、jpg, jpeg, gif, pngのものにしてください";
-          unlink($image_tmp["{$key}"]);
+          //unlink($image_tmp["{$key}"]);
         }
       }
     }
+    //画像サイズのチェック
     foreach ($image["size"] as $key => $value) {
-      //画像サイズを制限
+      //画像サイズを制限(5MB以下)
       if ($value > 5*1024*1024) {
       $error_msgs[] = "【{$image['name'][$key]}】画像サイズは、5MB以下にして下さい";
-      unlink($image_tmp["{$key}"]);
+      //unlink($image_tmp["{$key}"]);
       }
     }
   }
-  //新規登録時のみ画像必須
+  //新規登録時のみ画像必須、array_searchは配列中に該当データがなければfalseを返す
   if (empty($mode) && array_search(0, $image["error"]) === false) $error_msgs[] = "画像を選択してください";
   if ($request["frame_link"] == "") $error_msgs[] = "商品リンクを入力してください";
   if ($request["frame_lens_width"] == "") $error_msgs[] = "レンズ幅を入力してください";
@@ -192,28 +201,24 @@ if (isset($request["send"]) && empty($error_msgs)) {
   print("<br>");
   var_export(array_search(0, $image["error"]) !== false);
   print("<br><br>");
+  //画像のアップロードがある時の処理開始
   if (array_search(0, $image["error"]) !== false){
-    /*
-    //フレーム時のみ古い画像を削除
-    if ($mode == "change") {
-      //古い画像削除
-      unlink("../images/frames/{$_SESSION["old_image"]}");
-      unlink("../images/frames/thumb_{$_SESSION["old_image"]}");
-    }
-    */
+    //画像が複数ある時、$_FILESは多次元の連想配列になり、値に格納される配列のインデックスがファイルの番号と関係するので、ファイルの数count($image_tmp)分forで回す
     for ($i = 0; $i < count($image_tmp); ++$i) {
       print('$image["error"][$i] === 0');
       print("<br>");
       var_export($image['error'][$i] === 0);
       print("<br><br>");
+      //画像データの投稿がある時のみ実行
       if ($image['error'][$i] === 0) {
+        //本物の画像拡張子をgetimagesizeでゲットし、拡張子だけ(jpg, png, gif等)に整形
         //getimagesize($image_tmp['tmp_name'][0,1,2...])
         $image_extension = str_replace("image/", "", getimagesize($image_tmp[$i])["mime"]);
         print('$image_tmp[$i]');
         print("<br>");
         var_export($image_tmp[$i]);
         print("<br><br>");
-        //画像リソースを作成
+        //画像リソースを拡張子に応じて作成
         //jpeg, jpg
         if ($image_extension == "jpeg" || $image_extension == "jpg") $original_image = imagecreatefromjpeg($image_tmp[$i]);
         //png
@@ -240,11 +245,16 @@ if (isset($request["send"]) && empty($error_msgs)) {
         $thumb_image = imagecreatetruecolor($thumb_w, $thumb_h);
         imagecopyresized($thumb_image, $original_image, 0, 0, 0, 0, $thumb_w, $thumb_h, $original_w, $original_h);
         imagejpeg($thumb_image, "../images/frames/thumb_{$image_names[$i]}");
+        //画像リソースを解放、オリジナル画像のリソース・メイン画像のリソース・サムネ画像のリソース
+        imagedestroy($original_image);
+        imagedestroy($resized_image);
+        imagedestroy($thumb_image);
       }
     }
     //古い画像削除
-    unlink("../images/frames/{$_SESSION["old_image"]}");
-    unlink("../images/frames/thumb_{$_SESSION["old_image"]}");
+    //unlink("../images/frames/{$_SESSION["old_image"]}");
+    //unlink("../images/frames/thumb_{$_SESSION["old_image"]}");
+    deleteImages($_SESSION["old_image"]);
   }
 /*=============================================================================
     <<データベース更新
@@ -277,8 +287,8 @@ if (isset($request["send"]) && empty($error_msgs)) {
       $stmt->bindValue(":frame_id", $request["frame_id"], PDO::PARAM_INT);
       $stmt->bindValue(":frame_price", $request["frame_price"], PDO::PARAM_INT);
       //画像の更新があれば新しいのに、なければ古い画像のまま
-      if ($image["error"] == 0) {
-        $stmt->bindValue(":frame_image", $image_name, PDO::PARAM_STR);
+      if (array_search(0, $image["error"]) !== false) {
+        $stmt->bindValue(":frame_image", $image_names_imploded, PDO::PARAM_STR);
       } else {
         $stmt->bindValue(":frame_image", $_SESSION["old_image"], PDO::PARAM_STR);
       }
@@ -363,12 +373,15 @@ if (isset($request["send"]) && empty($error_msgs)) {
 /*=============================================================================
     データベース更新>>
 =============================================================================*/
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    送信ボタンが押されて、エラーメッセージがない時、新規登録or修正終了>>
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 /*-----------------------------------------------------------------------------
     アップデート後の各値を取得(エラー時の各値を取得)
 -----------------------------------------------------------------------------*/
 } else if (!empty($error_msgs)) {
   $row_frame['frame_price'] = $request["frame_price"];
-  $row_frame["frame_image"] = $request["frame_image"];
+  //$row_frame["frame_image"] = $request["frame_image"];
   $row_frame["frame_link"] = $request["frame_link"];
   $row_frame["frame_lens_width"] = $request["frame_lens_width"];
   $row_frame["frame_lens_height"] = $request["frame_lens_height"];
@@ -376,9 +389,23 @@ if (isset($request["send"]) && empty($error_msgs)) {
   $row_frame["frame_temple_length"] = $request["frame_temple_length"];
   $row_frame["frame_frame_width"] = $request["frame_frame_width"];
 }
-/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    送信ボタンが押されて、エラーメッセージがない時、新規登録or修正終了>>
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+/*-----------------------------------------------------------------------------
+    データベースから得た画像データの文字列をループようにexplodeして配列にする、序でにキーにfigcaptionで使うものを格納
+-----------------------------------------------------------------------------*/
+$row_frame["frame_image"] = toggleStrArray($row_frame["frame_image"]);
+foreach ($row_frame["frame_image"] as $key => $value) {
+  //1枚目の時、イメージ用配列を初期化、キャプションをメインにする
+  if ($key === 0) {
+    $row_frame["frame_image"] = array();
+    $key = "メイン";
+  } else {
+    $key = ++$key . "枚目";
+  }
+  //イメージのループ用連想配列
+  $row_frame["frame_image"][$key] = $value;
+}
+//var_export($row_frame["frame_image"]);
+//var_export($row_frame["frame_image"]["メイン"]);
 $page_title = "フレーム編集";
 require("header.php");
 ?>
@@ -403,10 +430,15 @@ require("header.php");
         <input type="number" name="frame_price" id="kakaku" max="99999" value="<?= he($row_frame['frame_price']); ?>">
       </div>
       <div class="image-upload">
-        <label for="gazou">画像<span class="attention">*</span></label>
+        <label for="gazou">画像<span class="attention">*</span>(画像1枚5MB以内、10枚まで登録できます)</label>
         <?php if ($row_frame["frame_image"]): ?>
-        <p><img src="<?= '../images/frames/' . he($row_frame["frame_image"]) ?>"></p>
-      <?php endif; ?>
+        <?php foreach ($row_frame["frame_image"] as $key => $value) : ?>
+        <figure>
+          <figcaption><?= $key ?></figcaption>
+          <img src="<?= '../images/frames/' . he($value) ?>">
+        </figure>
+        <?php endforeach; ?>
+        <?php endif; ?>
       <input type="file" name="frame_image[]" id="aikon" accept="image/png, image/jpeg, image/gif">
         <div class="selected-images-result"></div>
       </div>
@@ -436,7 +468,7 @@ require("header.php");
       </div>
       <div>
         <input type="submit" name="send" value="送信する">
-        <input type="hidden" name="MAX_FILE_SIZE" value="5242880" />
+        <input type="hidden" name="MAX_FILE_SIZE" value="5242880">
         <input type="hidden" name="mode" value="<?= he($mode); ?>">
         <input type="hidden" name="frame_id" value="<?= he($request['frame_id']); ?>">
       </div>
